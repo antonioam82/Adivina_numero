c53ccc8e1ad17ad06025d216a31436e725e8dd9d
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2024-07-17 16:23:04 +0200

    Update adivina_numero.cpp

--- adivina_numero.cpp
+++ adivina_numero.cpp
@@ -3,8 +3,58 @@
 
 using namespace std;
 
+int seleccionarDificultad(){
+    bool opcion_valida = false;
+    int nivelDificultad;
+
+    while(opcion_valida == false){
+       cout << "********DIFICULTAD********" << endl;
+       cout << "NIVEL 1: ENTRE 0 Y 100" << endl;
+       cout << "NIVEL 2: ENTRE 0 Y 1000" << endl;
+       cout << "NIVEL 3: ENTRE 0 Y 10000" << endl;
+       cout << "NIVEL 4: ENTRE 0 Y 100000" << endl;
+       cout << "Introduzca nivel de dificultad: ";
+       cin >> nivelDificultad;
+
+       if(cin.fail()) {
+            cin.clear(); // Limpia el estado de error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descarta la entrada inválida
+            cout << "OPCION INVALIDA. Por favor, ingrese un numero entero (1, 2, 3 o 4)." << endl;
+            opcion_valida = false; // Restablecer 'opcion' para mantener el bucle
+        }else{
+            switch(nivelDificultad){
+                case 1:
+                    cout << "NIVEL 1" << endl;
+                    opcion_valida = true;
+                    break;
+                case 2:
+                    cout << "NIVEL 2" << endl;
+                    opcion_valida = true;
+                    break;
+                case 3:
+                    cout << "NIVEL 3" << endl;
+                    opcion_valida = true;
+                    break;
+                case 4:
+                    cout << "NIVEL 4" << endl;
+                    opcion_valida = true;
+                    break;
+                default:
+                    cout << "OPCION NO VALIDA" << endl;
+                    opcion_valida = false;
+                    break;
+
+
+            }
+        }
+
+    }
+    return nivelDificultad;
+}
+
 int main() {
     int opcion = 1;
+    int dificultad;
 
     while(opcion != 2) {
         cout << "*****ADIVINA NUMERO****" << endl;
@@ -23,6 +73,9 @@ int main() {
                 case 1:
                     cout << "Ha escogido jugar" << endl;
                     // Aquí puedes agregar el código del juego
+                    dificultad = seleccionarDificultad();
+                    cout << "LA DIFICULTAD ESCOJIDA ES " << dificultad << " :)" << endl;
+
                     break;
                 case 2:
                     cout << "Ha elegido salir" << endl;
